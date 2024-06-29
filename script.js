70eb98e1a042bc3853815ed317d6a4a6fe69d95a
keomoniroath <keomoniroath.tao@gmail.com>
2024-06-29 16:08:56 +0200

    add autoplay to carousel

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 
 var slideIndex = 1;
+var slides = document.getElementsByClassName("slide");
+var number = document.getElementsByClassName("number");
 showSlides(slideIndex);
 
 function nextSlide(n) {
@@ -12,20 +14,41 @@ function currentSlide(n) {
 
 function showSlides(n) {
     var i;
-    var slides = document.getElementsByClassName("slide");
-    var dots = document.getElementsByClassName("dot");
     if (n > slides.length) {slideIndex = 1}
     if (n < 1) {slideIndex = slides.length}
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
+    for (i = 0; i < number.length; i++) {
+        number[i].className = number[i].className.replace(" active", "");
     }
     slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
+    number[slideIndex-1].className += " active";
 }
 
+// Autoplay to carousel
+setInterval(autoPlay, 4000);
+function autoPlay() {
+    var activeSlide = document.getElementsByClassName("active-slide");
+    var id = activeSlide[0].id;
+    var nextId;
+
+    if(id == 3) {
+        nextId = 0;
+    } else {
+        nextId = parseInt(id) + 1;
+    }
+
+    activeSlide[0].style.display = "none";
+    activeSlide[0].className = activeSlide[0].className.replace(" active-slide", "");
+    slides[nextId].style.display = "block";
+    slides[nextId].className += " active-slide";
+    number[nextId].className += " active";
+    number[id].className = number[id].className.replace(" active", "");
+
+}
+
+// Show timeline content on click
 var icon = document.getElementsByClassName("icon");
 for (var i = 0 ; i < icon.length; i++) {
     icon[i].addEventListener('click' , displayTimelineContent , false ); 
@@ -52,6 +75,7 @@ function displayTimelineContent() {
     }
 }
 
+//Remove active state for middle news
 var news = document.getElementsByClassName("news-container");
 for (var i = 0 ; i < news.length; i++) {
     news[i].addEventListener('mouseover' , displayNewsContent , false ); 
@@ -64,6 +88,7 @@ function displayNewsContent() {
     } 
 }
 
+//Remove active state for first tab
 var card = document.getElementsByClassName("card");
 var cardIsActive = document.getElementsByClassName("card active");
 for (var i = 0 ; i < card.length; i++) {
